376d8156496c2ead2730cb69a1c3ca5e8ac51678
Alejandro Piguave <alejandro.piguave.illescas@hotmail.com>
2019-09-24 20:35:47 +0200

    Testing changes

---
+++ Sample App/Sample App/UI/Controllers/MainViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewController.swift
+//  Sample App
+//
+//  Created by Dev2 on 24/09/2019.
+//  Copyright © 2019 Dev2. All rights reserved.
+//
+import UIKit
+
+class MainViewController: UIViewController {
+    
+}
